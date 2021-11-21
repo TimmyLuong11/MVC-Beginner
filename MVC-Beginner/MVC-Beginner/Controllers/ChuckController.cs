be54e0f419558838cb7ea6ddd0f5022c2ad62684
TimmyLuong <timmyluong11@gmail.com>
2021-11-21 16:09:15 -0600

    Finished MVC

---
+++ MVC-Beginner/MVC-Beginner/Controllers/ChuckController.cs
@@ -0,0 +1,28 @@
+﻿using MVC_Beginner.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Beginner.Controllers
+{
+    public class ChuckController : Controller
+    {
+        // GET: Chuck
+        public ActionResult Index()
+        {
+            RandomChuckJokeAPI joke;
+
+            using (var client = new HttpClient())
+            {
+                string jsonData = client.GetStringAsync("http://api.chucknorris.io/jokes/random").Result;
+                joke = JsonConvert.DeserializeObject<RandomChuckJokeAPI>(jsonData);
+            }
+
+            return View(joke);
+        }
+    }
+}
---
+++ MVC-Beginner/MVC-Beginner/Controllers/PokemonController.cs
@@ -0,0 +1,41 @@
+﻿using MVC_Beginner.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Beginner.Controllers
+{
+    public class PokemonController : Controller
+    {
+        // GET: Pokemon
+        public ActionResult Index()
+        {
+            PokemonAPI allPokemon;
+
+            using (var client = new HttpClient())
+            {
+                string jsonData = client.GetStringAsync("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1118").Result;
+                allPokemon = JsonConvert.DeserializeObject<PokemonAPI>(jsonData);
+            }
+
+            return View(allPokemon.results);
+        }
+
+        public ActionResult Info(string id)
+        {
+            PokemonInfo info;
+
+            using (var client = new HttpClient())
+            {
+                string jsonData = client.GetStringAsync($"https://pokeapi.co/api/v2/pokemon/{id}").Result;
+                info = JsonConvert.DeserializeObject<PokemonInfo>(jsonData);
+            }
+
+            return View(info);
+        }
+    }
+}
